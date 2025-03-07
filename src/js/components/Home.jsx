bb5f3ed6030b0757e343c979a403495747530a2e
Alex Ayala Palacin <95265085+aayalapalacin@users.noreply.github.com>
2025-03-07 22:54:07 +0000

    added beginning exercise

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
+import  Modal from  "./Modal";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [samepleState, setSampleState]= useState("");
+	
 	return (
 		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+           <button>Orangle</button>
+           <button>Apple</button>
+           <button>Broccoli</button>
+		   <p>I like, I like, to eat nothing</p>
+		<Modal />
 		</div>
 	);
 };
