eda0ca7bf291dd50a9b3e019ba4bf6da21bbaf3b
Alex Ayala Palacin <95265085+aayalapalacin@users.noreply.github.com>
2025-03-07 22:57:59 +0000

    filter exerise

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -4,11 +4,22 @@ import  Modal from  "./Modal";
 const Home = () => {
 	const [samepleState, setSampleState]= useState("");
 	
+	const fruitArray = ["Organge", "Apple", "Broccoli"];
+
+	const fruitNoOrange = [];
+	const fruitNoApple = [];
+	const fruitNoBroccoli = [];
+	
 	return (
 		<div className="text-center">
-           <button>Orangle</button>
-           <button>Apple</button>
-           <button>Broccoli</button>
+			{fruitArray.map((fruit)=>{
+				return(
+					<div>
+						<button>{fruit}</button>
+					</div>
+				)
+			})}
+
 		   <p>I like, I like, to eat nothing</p>
 		<Modal />
 		</div>
